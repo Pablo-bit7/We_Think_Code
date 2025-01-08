3a558f32830e259835119a61540f11afae419e6b
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2025-01-08 20:50:52 +0200

    And that why we avoid repetitious code...

--- features/team_allocator.py
+++ features/team_allocator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
-    This is the team allocator project solution example project
-'''
+"""
+    Team Allocator Module
+"""
 
 
 def student_list():
@@ -27,170 +27,272 @@ def student_list():
 
 
 def dbn_campus_students(student_list):
-    '''
-    from the list of students above, fill in this function to return a list of all
-    students in the Durban campus only.
-    '''
+    """
+    Filters for attendees at the Durban campus from the full student list.
+    """
     dbn_students = []
+    for student in student_list:
+        if "Durban" in student:
+            dbn_students.append(student)
 
-    return dbn_students
+    return (dbn_students)
 
 
 def dbn_physical_students(dbn_students):
-    '''
-    from the list of dbn_campus_students, fill in this function to return a list of all
-    students who will be attending physically on campus
-    '''
+    """
+    Filters for physical attendees at the Durban campus.
+    """
+    dbn_physical_students_list = []
+    for student in dbn_students:
+        if "Physical" in student:
+            dbn_physical_students_list.append(student)
 
-    return dbn_physical_students
-
-
-def dbn_physical_teams(dbn_physical_students):
-    '''
-    from the list of dbn_physical_students create list of 4 students per team, and add them to 
-    one big list
-    '''
-
-    return dbn_physical_teams
+    return (dbn_physical_students_list)
 
 
-def dbn_teams_file(durban_physical_teams):
-    '''
-    write and save the information in the dbn_physical_teams into a textfile
-    '''
+def dbn_physical_teams(dbn_physical_students_list):
+    """
+    Divides the physical Durban attendees into teams of 4.
+    """
+    dbn_physical_teams_list = []
+    team = []
+
+    for student in dbn_physical_students_list:
+        team.append(student)
+        if len(team) == 4:
+            dbn_physical_teams_list.append(team)
+            team = []  # reset
+
+    if team:
+        dbn_physical_teams_list.append(team)
+
+    return (dbn_physical_teams_list)
+
+
+def dbn_teams_file(durban_physical_teams_list):
+    """
+    Writes the physical Durban teams into a text file.
+    """
+    with open("dbn_teams.txt", "w") as file:
+        for team_number, team in enumerate(dbn_physical_teams_list, start=1):
+            file.write(f"Team {team_number}:\n")
+            for student in team:
+                file.write(f"  {student}\n")
+            file.write("\n")  # blank line between teams
 
 
 def cpt_campus_students(student_list):
-    '''
-    from the list of students above, fill in this function to return a list of all
-    students in the Cape Town campus only.
-    '''
+    """
+    Filters for attendees at the Cape Town campus from the full sttudent list.
+    """
     cpt_students = []
+    for student in student_list:
+        if "Cape Town" in student:
+            cpt_students.append(student)
 
-    return cpt_students
+    return (cpt_students)
 
 
-def cpt_physical_students(cpt_physical_students):
-    '''
-    from the list of cpt_campus_students, fill in this function to return a list of all
-    students who will be attending physically on campus
-    '''
+def cpt_physical_students(cpt_students):
+    """
+    Filters for the physical attendees at the Cape Town campus.
+    """
+    cpt_physical_students_list = []
+    for student in cpt_students:
+        if "Physical" in student:
+           cpt_physical_students_list.append(student) 
     
-    return cpt_physical_students
+    return (cpt_physical_students_list)
 
 
-def cpt_physical_teams(cpt_physical_teams):
-    '''
-    from the list of cpt_physical_students create list of 4 students per team, and add them to 
-    one big list
-    '''
-    
-    return cpt_physical_teams
+def cpt_physical_teams(cpt_physical_students_list):
+    """
+    Divides the physical Cape Town atendees into teams of 4.
+    """
+    cpt_physical_teams_list =[]
+    team = []
+
+    for student in cpt_physical_students_list:
+        team.append(student)
+        if len(team) == 4:
+            cpt_physical_teams_list.append(team)
+            team = []
+
+    if team:
+        cpt_physical_teams_list.append(team)
+
+    return (cpt_physical_teams_list)
 
 
-def cpt_teams_file(capetown_final_teams):
-    '''
-    write and save the information in the cpt_physical_teams into a textfile
-    '''
+def cpt_teams_file(cpt_physical_teams_list):
+    """
+    Writes the physical Cape Town teams into a text file.
+    """
+    with open("cpt_teams.txt", "w") as file:
+        for team_number, team in enumerate(cpt_physical_teams_list, start=1):
+            file.write(f"Team {team_number}:\n")
+            for student in team:
+                file.write(f"  {student}\n")
+            file.write("\n")
 
 
 def jhb_campus_students(student_list):
-    '''
-    from the list of students above, fill in this function to return a list of all
-    students in the Johannesburg campus only.
-    '''
+    """
+    Filters for attendees at the Johannesburg campus from the full student list.
+    """
     jhb_students = []
+    for student in student_list:
+        if "Johannesburg" in student:
+            jhb_students.append(student)
 
-    return jhb_students
-
-
-def jhb_physical_students(jhb_physical_students):
-    '''
-    from the list of jhb_campus_students, fill in this function to return a list of all
-    students who will be attending physically on campus
-    '''
-
-    return jhb_physical_students
+    return (jhb_students)
 
 
-def jhb_physical_teams(jhb_physical_teams):
-    '''
-    from the list of jhb_physical_students create list of 4 students per team, and add them to 
-    one big list
-    '''
+def jhb_physical_students(jhb_students):
+    """
+    Filters for physical attendees at the Johannesburg campus 
+    """
+    jhb_physical_students_list = []
+    for student in jhb_students:
+        if "Physical" in student:
+            jhb_physical_students_list.append(student)
 
-    return jhb_physical_teams
+    return (jhb_physical_students_list)
 
 
-def jhb_teams_file(jhb_final_teams):
-    '''
-    write and save the information in the jhb_physical_teams into a textfile
-    '''
+def jhb_physical_teams(jhb_physical_students_list):
+    """
+    Divides the physical attendees at the Johannesburg campus into teams of 4.
+    """
+    jhb_physical_teams_list = []
+    team = []
+
+    for student in jhb_physical_students_list:
+        team.append(student)
+        if len(team) == 4:
+            jhb_physical_teams_list.append(team)
+            team = []
+
+    if team:
+        jhb_physical_teams_list.append(team)
+
+    return (jhb_physical_teams_list)
+
+
+def jhb_teams_file(jhb_physical_teams_list):
+    """
+    Writes the physical Johannesburg teams into a text file.
+    """
+    with open("jhb_teams.txt", "w") as file:
+        for team_number, team in enumerate(jhb_physical_teams_list, start=1):
+            file.write(f"Team {team_number}:\n")
+            for student in team:
+                file.write(f"  {student}\n")
+            file.write("\n")
 
 
 def nw_campus_students(student_list):
-    '''
-    from the list of students above, fill in this function to return a list of all
-    students in the North West campus only.
-    '''
+    """
+    Filters for attendees at the North West campus from the full student list.
+    """
     nw_students = []
+    for student in student_list:
+        if "Phokeng" in student:
+            nw_students.append(student)
 
-    return nw_students
-
-
-def nw_physical_students(nw_physical_students):
-    '''
-    from the list of nw_campus_students, fill in this function to return a list of all
-    students who will be attending physically on campus
-    '''
-
-    return nw_physical_students
+    return (nw_students)
 
 
-def nw_physical_teams(nw_physical_teams):
-    '''
-    from the list of nw_physical_students, create list of 4 students per team, and add them to 
-    one big list
-    '''
+def nw_physical_students(nw_students):
+    """
+    Filters for physical attendees at the North West campus.
+    """
+    nw_physical_students_list = []
+    for student in nw_students:
+        if "Physical" in student:
+            nw_physical_students_list.append(student)
 
-    return nw_physical_teams
-
-
-def nw_teams_file(nw_final_teams):
-    '''
-    write and save the information in the nw_physical_teams into a textfile
-    '''
+    return (nw_physical_students_list)
 
 
-def get_virtual_students():
-    '''
-    from the list of students above, fill in this function to return a list of all
-    students attending virtually.
-    '''
+def nw_physical_teams(nw_physical_students_list):
+    """
+    Divides the physical attendees at the North West campus into teams of 4.
+    """
+    nw_physical_teams_list = []
+    team = []
+
+    for student in nw_physical_students_list:
+        team.append(student)
+        if len(team) == 4:
+            nw_physical_teams_list.append(team)
+            team = []
+
+    if team:
+        nw_physical_teams_list.append(team)
+
+    return (nw_physical_teams_list)
+
+
+def nw_teams_file(nw_physical_teams_list):
+    """
+    Writes the physical teams at the North West campus into a text file.
+    """
+    with open("nw_teams.txt", "w") as file:
+        for team_number, team in enumerate(nw_physical_teams_list, start=1):
+            file.write(f"Team {team_number}:\n")
+            for student in team:
+                file.write(f"  {student}\n")
+            file.write("\n")
+
+
+def get_virtual_students(student_list):
+    """
+    Filters for the virtual attendees from the full student list.
+    """
     virtual_campus = []
+    for student in student_list:
+        if "Virtual" in student:
+            virtual_campus.append(student)
 
-    return virtual_campus
-
-
-def virtual_teams():
-    '''
-    from the list of virtual_students above,  create list of 4 students per team, and add them to 
-        one big list
-    '''
-    virtual_teams = []
-
-    return virtual_teams
+    return (virtual_campus)
 
 
-def virtual_teams_file():
-    '''
-    write and save the information in the virtual_teams into a textfile
-    '''
+def virtual_teams(virtual_campus):
+    """
+    Divides the virtual attendees into teams of 4.
+    """
+    virtual_teams_list = []
+    team = []
+
+    for student in virtual_campus:
+        team.append(student)
+        if lean(team) == 4:
+            virtual_teams_list.append(team)
+            team = []
+
+    if team:
+        virtual_teams_list.append(team)
+
+    return (virtual_teams_list)
+
+
+def virtual_teams_file(virtual_teams_list):
+    """
+    Writes the virtual teams into a text file
+    """
+    with open("vr_teams.txt", "w") as file:
+        for team_number, team in enumerate(virtual_teams_list, start=1):
+            file.write(f"Team {team_number}:\n")
+            for student in team:
+                file.write(f"  {student}\n")
+            file.write("\n")
 
 
 if __name__ == '__main__':
-    '''
-    call all your functions below to make your program execute    
-    '''
-    pass
+    students = student_list()
 
+    dbn_students = dbn_campus_students(students)
+    physical_dbn_students = dbn_physical_students(dbn_students)
+    dbn_teams = dbn_physical_teams(physical_dbn_students)
+    dbn_teams_file(dbn_teams)
--- unit_tests/test_team_allocator.py
+++ unit_tests/test_team_allocator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import unittest
 from team_allocator import *
 

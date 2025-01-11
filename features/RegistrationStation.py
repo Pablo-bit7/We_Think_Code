ea785ca37d0b0aa110f733bfd3293298d5193e2f
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2025-01-12 01:56:10 +0200

    test_RegistrationStation.py readable

--- features/RegistrationStation.py
+++ features/RegistrationStation.py
@@ -1,51 +1,54 @@
-# Registration Station project
-
-
-def read_file(data_flt):
-
-    # Read and return contents of text file
-    
-    text_file = open("C:\\Users\\mogan\\Documents\\ThinkCode\\Bootcamp Helper Resources\\skeleton_code\\bootcampers.txt","r+")
-    data_flt = text_file.readlines()
-    return data_flt
+#!/usr/bin/env python3
+"""
+Registration Station project
+"""
 
 
 def input_user_name():
-
     """
     Takes username as input
     """
 
+    return (user_name)
+
+
+def get_file_contents():
+    """
+    Return desired text file
+    """
+
+    return (file_name)
+
+
+def read_file(file_name):
+    """
+    Read and return contents of text file
+    """
+
+    return (file_data)
+
+
+def find_username(user_name, file_data):
+    """
+    Main functiontion for running Registration Station, which inlcude:
+       * get username input from user
+       * check if username exists and print corresponding details
+    """
+
 
 def correct_or_incorrect():
-
     """
     Prompt to ask if details are correct or not
     """
 
 
 def correct_details():
-
     """
     Prompt to correct the user details in text file, which includes:
     * Username
     * Date
     * Location
     * Experience
-    """
-
-
-def get_file_contents():
-
-    """Return desired text file"""
-
-
-def find_username(file_name):
-
-    """
-    Main functiontion for running Registration Station, which inlcude:
-       * get username input from user
-       * check if username exists and print corresponding details
     """
 
 
--- unit_tests/test_RegistrationStation.py
+++ unit_tests/test_RegistrationStation.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Unit tests for the Registration Station.
+"""
 import unittest
 from io import StringIO
 import sys
@@ -6,98 +10,142 @@ import RegistrationStation
 
 
 class MyTestCase(unittest.TestCase):
+    """
+    Test cases for validating the Registration Station feature.
+    """
 
     @patch("sys.stdin", StringIO("elomkhDBN2022\ny\n"))
     def test_valid_username_lowercase(self):
-
+        """
+        Test valid lowercase username input.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
-        self.assertEqual("Select username: 4 April - Johannesburg Physical - No prior experience\n", text_capture.getvalue())
+        self.assertEqual(
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elomkhDBN2022\nY\n"))
     def test_valid_username_Uppercase(self):
-
+        """
+        Test valid uppercase username input.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
-        self.assertEqual("Select username: 4 April - Johannesburg Physical - No prior experience\n", text_capture.getvalue())
-
+        self.assertEqual(
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elokhDBN2022\nelomkhDBN2022\ny\n"))
-    def test__invalid_username(self):
-
+    def test_invalid_username(self):
+        """
+        Test invalid username followed by a valid username.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
-        self.assertEqual("""Select username: Please enter valid existing username
-Select username: 4 April - Johannesburg Physical - No prior experience\n""", text_capture.getvalue())
+        self.assertEqual(
+            "Select username: Please enter valid existing username\n"
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elomkhDBN2022\ny\n"))
     def test_valid_confirmation(self):
-
+        """
+        Test confirmation of correct user details.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
         RegistrationStation.correct_or_incorrect()
-        self.assertEqual("""Select username: 4 April - Johannesburg Physical - No prior experience
-Is this correct? (Y/n): """, text_capture.getvalue())
+        self.assertEqual(
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n"
+            "Is this correct? (Y/n): ",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elonkhDBN2022\nelomkhDBN2022\ny\n"))
     def test_valid_confirmation_invalid_username(self):
-
+        """
+        Test valid confirmation after correcting an invalid username.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
         RegistrationStation.correct_or_incorrect()
-        self.assertEqual("""Select username: Please enter valid existing username
-Select username: 4 April - Johannesburg Physical - No prior experience
-Is this correct? (Y/n): """, text_capture.getvalue())
+        self.assertEqual(
+            "Select username: Please enter valid existing username\n"
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n"
+            "Is this correct? (Y/n): ",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elomkhDBN2022\nn\n"))
     def test_incorrect_user_details(self):
-
+        """
+        Test when user denies the correctness of their details.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
         RegistrationStation.correct_or_incorrect()
-        self.assertEqual("""Select username: 4 April - Johannesburg Physical - No prior experience
-Is this correct? (Y/n): """, text_capture.getvalue())
+        self.assertEqual(
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n"
+            "Is this correct? (Y/n): ",
+            text_capture.getvalue()
+        )
 
     @patch("sys.stdin", StringIO("elomkhDBN2022\nn\nelomkhDBN2022 - 4 April - Johannesburg Physical - No prior experience\n"))
     def test_incorrect_user_details_corrected(self):
-
+        """
+        Test correction of incorrect user details.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
         RegistrationStation.correct_or_incorrect()
         RegistrationStation.correct_details()
-        self.assertEqual("""Select username: 4 April - Johannesburg Physical - No prior experience
-Is this correct? (Y/n): Username - Date - Location - Experience: 4 April - Johannesburg Physical - No prior experience\n""", text_capture.getvalue())   
+        self.assertEqual(
+            "Select username: 4 April - Johannesburg Physical - No prior experience\n"
+            "Is this correct? (Y/n): Username - Date - Location - Experience: 4 April - Johannesburg Physical - No prior experience\n""",
+            text_capture.getvalue()
+        )
  
  
     @patch("sys.stdin", StringIO("colootsJHB2023\nn\ncolootsJHB2023 - 13 May - Johannesburg Physical - No Prior Experience\n"))
     def test_incorrect_user_details_corrected_additional(self):
-
+        """
+        Test correction of details for a different username.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
         RegistrationStation.find_username('bootcampers.txt')
         RegistrationStation.correct_or_incorrect()
         RegistrationStation.correct_details()
-        self.assertEqual("""Select username: 13 May - Johannesburg Physical - No Prior Experience
-Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience\n""", text_capture.getvalue())   
+        self.assertEqual(
+            "Select username: 13 May - Johannesburg Physical - No Prior Experience\n"
+            "Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience\n",
+            text_capture.getvalue()
+        )
  
     @patch("sys.stdin", StringIO("colootsJHB2023\nn\ncolootsJHB2023 - 13 May - Johannesburg Physical - No Prior Experience\nn\ncolootsJHB2023 - 13 May - Johannesburg Physical - No Prior Experience\ny"))
     def test_incorrect_user_details_incorrect_addition(self):
-
+        """
+        Test multiple incorrect corrections before confirming.
+        """
         text_capture = StringIO()
         sys.stdout = text_capture
 
@@ -106,9 +154,13 @@ Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johann
         RegistrationStation.correct_details()
         RegistrationStation.correct_or_incorrect()
         RegistrationStation.correct_details()
-        self.assertEqual("""Select username: 13 May - Johannesburg Physical - No Prior Experience
-Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience
-Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience\n""", text_capture.getvalue())   
- 
+        self.assertEqual(
+            "Select username: 13 May - Johannesburg Physical - No Prior Experience\n"
+            "Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience\n"
+            "Is this correct? (Y/n): Username - Date - Location - Experience: 13 May - Johannesburg Physical - No Prior Experience\n",
+            text_capture.getvalue()
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
